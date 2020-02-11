b85b56b1adecaadd8c7c52b8f84b4b7518617d58
K  L Shamanth <60220114+Shamanth-KL@users.noreply.github.com>
2020-02-11 19:15:53 +0000

    Add files via upload

---
+++ BankAccount_Week2/src/AccountTest.java
@@ -0,0 +1,18 @@
+public class AccountTest {
+        public static void main(String[] args) {
+                SavingsAccount momsSavings = new SavingsAccount(0.5);
+                CheckingAccount harrysChecking = new CheckingAccount(100);
+                momsSavings.deposit(10000);
+                momsSavings.transfer(harrysChecking, 2000);
+                harrysChecking.withdraw(1500);
+                harrysChecking.withdraw(80);
+                momsSavings.transfer(harrysChecking, 1000);
+                harrysChecking.withdraw(400);
+                momsSavings.addInterest();
+                harrysChecking.deductFees();
+                System.out.println("Mom's savings balance = $" + momsSavings.getBalance());
+                System.out.println("Harry's checking balance = $" + harrysChecking.getBalance());
+
+        }
+
+}
---
+++ BankAccount_Week2/src/CheckingAccount.java
@@ -0,0 +1,34 @@
+public class CheckingAccount extends BankAcc {
+        public CheckingAccount(int initialBalance) {
+                super(initialBalance);
+                transactionCount = 0;
+        }
+
+        public void deposit(double amount) {
+                transactionCount++;
+                super.deposit(amount);
+        }
+
+        public void withdraw(double amount) {
+                transactionCount++;
+                super.withdraw(amount);
+        }
+
+        public void deductFees() {
+                if (transactionCount > FREE_TRANSACTIONS) {
+                        double fees = TRANSACTION_FEE * (transactionCount - FREE_TRANSACTIONS);
+                        super.withdraw(fees);
+                }
+                transactionCount = 0;
+        }
+
+        private int transactionCount;
+
+        private static final int FREE_TRANSACTIONS = 3;
+
+        private static final double TRANSACTION_FEE = 2.0;
+
+}
+
+
+
