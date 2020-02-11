d286220e96cf7958e6107392cd0c44eddceee763
K  L Shamanth <60220114+Shamanth-KL@users.noreply.github.com>
2020-02-11 19:49:56 +0000

    Add files via upload

---
+++ BankAccount_Week2/BankAccount_branch/src/BankAcc.java
@@ -0,0 +1,46 @@
+public class BankAcc implements Measurable  {
+
+    public BankAcc() {
+
+        balance = 0;
+
+    }
+    public BankAcc(double initialBalance) {
+
+        balance = initialBalance;
+
+    }
+
+    public void deposit(double amount) {
+
+        balance = balance + amount;
+
+    }
+
+    public void withdraw(double amount) {
+
+        balance = balance - amount;
+
+    }
+
+    public double getBalance() {
+
+        return balance;
+
+    }
+
+    public void transfer(BankAcc other, double amount) {
+
+        withdraw(amount);
+
+        other.deposit(amount);
+
+    }
+
+    private double balance;
+
+    @Override
+    public double getMeasure() {
+        return 0;
+    }
+}
