1497ebdad6fd4b9bb2ce758b9dee4303d2e3c543
K  L Shamanth <60220114+Shamanth-KL@users.noreply.github.com>
2020-02-11 19:22:49 +0000

    Update BankAcc.java

--- BankAccount_Week2/src/BankAcc.java
+++ BankAccount_Week2/src/BankAcc.java
@@ -1,42 +1,29 @@
 public class BankAcc {
-
+    
     public BankAcc() {
-
         balance = 0;
-
     }
+    
     public BankAcc(double initialBalance) {
-
         balance = initialBalance;
-
     }
 
     public void deposit(double amount) {
-
         balance = balance + amount;
-
     }
 
     public void withdraw(double amount) {
-
         balance = balance - amount;
-
     }
-
+    
     public double getBalance() {
-
         return balance;
-
     }
 
     public void transfer(BankAcc other, double amount) {
-
         withdraw(amount);
-
         other.deposit(amount);
-
     }
 
     private double balance;
-
 }
