9e791850ea36799a7b93ee3a2f707f58984abdf5
K  L Shamanth <60220114+Shamanth-KL@users.noreply.github.com>
2020-02-12 20:22:38 +0000

    Update BankAcc.java

--- BankAccount_Week2/BankAccount_branch/src/BankAcc.java
+++ BankAccount_Week2/BankAccount_branch/src/BankAcc.java
@@ -1,34 +1,35 @@
 public class BankAcc implements Measurable  {
-
+    //construts BankAccount with Zero balance
     public BankAcc() {
 
         balance = 0;
 
     }
+    //Constructs bank account with given balance. initial balance
     public BankAcc(double initialBalance) {
 
         balance = initialBalance;
 
     }
-
+    // Deposits money into the bank account.
     public void deposit(double amount) {
 
         balance = balance + amount;
 
     }
-
+    //Withdraws money from bank account.
     public void withdraw(double amount) {
 
         balance = balance - amount;
 
     }
-
+    //Gets current balance of bank account.
     public double getBalance() {
 
         return balance;
 
     }
-
+    //Transfers money from bank account to another account
     public void transfer(BankAcc other, double amount) {
 
         withdraw(amount);
