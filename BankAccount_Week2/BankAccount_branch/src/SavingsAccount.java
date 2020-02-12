533338d8e4f128ec4b5cf11dc3d6078a4b4bc7ce
K  L Shamanth <60220114+Shamanth-KL@users.noreply.github.com>
2020-02-12 20:28:11 +0000

    Update SavingsAccount.java

--- BankAccount_Week2/BankAccount_branch/src/SavingsAccount.java
+++ BankAccount_Week2/BankAccount_branch/src/SavingsAccount.java
@@ -1,14 +1,23 @@
 public class SavingsAccount extends BankAcc {
-    public SavingsAccount(double rate)
-    {
+    public SavingsAccount(double rate){
         interestRate = rate;
     }
+    
+     //Constructs bank account with given balance and interest rate
+	public SavingsAccount(double initialBalance, double rate) {
+		super(initialBalance);
+		interestRate = rate;
+	}
 
-    public void addInterest()
-    {
+    public void addInterest(){
         double interest = getBalance() * interestRate / 100;
         deposit(interest);
     }
+    
+    public String toString(){
+		return "type SavingsAccount – balance £" + this.getBalance();
+	}
+
 
     private double interestRate;
 }
