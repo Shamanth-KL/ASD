1cdb5d4e18b9b116feb09fea4c1a7c481be7a006
K  L Shamanth <60220114+Shamanth-KL@users.noreply.github.com>
2020-02-12 20:16:46 +0000

    Update AccountTest.java

--- BankAccount_Week2/BankAccount_branch/src/AccountTest.java
+++ BankAccount_Week2/BankAccount_branch/src/AccountTest.java
@@ -1,15 +1,24 @@
-public class AccountTest {
-        public static void main(String[] args) {
+public class AccountTest 
+{
+        public static void main(String[] args) 
+        {
                 SavingsAccount momsSavings = new SavingsAccount(0.5);
+                
                 CheckingAccount harrysChecking = new CheckingAccount(100);
+                
                 momsSavings.deposit(10000);
                 momsSavings.transfer(harrysChecking, 2000);
+                
                 harrysChecking.withdraw(1500);
                 harrysChecking.withdraw(80);
+                
                 momsSavings.transfer(harrysChecking, 1000);
                 harrysChecking.withdraw(400);
+                
+                //simulates end of month balance
                 momsSavings.addInterest();
                 harrysChecking.deductFees();
+                
                 System.out.println("Mom's savings balance = $" + momsSavings.getBalance());
                 System.out.println("Harry's checking balance = $" + harrysChecking.getBalance());
 
