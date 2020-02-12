747c6e78f72be45599ed95cc1a80593cb9e9fcaf
K  L Shamanth <60220114+Shamanth-KL@users.noreply.github.com>
2020-02-12 20:24:34 +0000

    Update CheckingAccount.java

--- BankAccount_Week2/BankAccount_branch/src/CheckingAccount.java
+++ BankAccount_Week2/BankAccount_branch/src/CheckingAccount.java
@@ -1,7 +1,6 @@
-/**
- *
- */
 public class CheckingAccount extends BankAcc {
+        
+        //Constructs checking account
         public CheckingAccount(int initialBalance) {
                 super(initialBalance);
                 transactionCount = 0;
@@ -9,6 +8,8 @@ public class CheckingAccount extends BankAcc {
 
         public void deposit(double amount) {
                 transactionCount++;
+                
+                // construct superclass
                 super.deposit(amount);
         }
 
